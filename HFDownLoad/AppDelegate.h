1255141ab77e8117d8119ae8926e7fc39aed9a1b
勤奋的笨老头 <627999959@qq.com>
2015-09-07 09:37:23 +0800

    Initial Commit

---
+++ HFDownLoad/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HFDownLoad
+//
+//  Created by 洪峰 on 15/9/7.
+//  Copyright (c) 2015年 洪峰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HFDownLoad/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HFDownLoad
+//
+//  Created by 洪峰 on 15/9/7.
+//  Copyright (c) 2015年 洪峰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
